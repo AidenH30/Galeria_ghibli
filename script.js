a136096119a9b9687c86ffa8f34628c982edf911
Guilherme Almeida <102252950+AidenH30@users.noreply.github.com>
2022-07-28 18:53:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const search = document.querySelector(".searchbar input"),
+      images = document.querySelectorAll(".image-box");
+
+search.addEventListener("keyup", e =>{
+    if(e.key == "Enter"){
+        let searcValue = search.value,
+            value = searcValue.toLowerCase();
+            images.forEach(image =>{
+                if(value === image.dataset.name){
+                    return image.style.display = "block";
+                }
+                image.style.display = "none";
+         });
+    }
+});
+
+search.addEventListener("keyup", () =>{
+    if(search.value != "") return;
+    images.forEach(image =>{
+        image.style.display = "block";
+    })
+})
